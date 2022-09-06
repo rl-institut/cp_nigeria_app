d927d03ac64e69af85c5e858769be49c4f5eda02
paulapreuss <p.preuss@tu-berlin.de>
2022-09-06 11:35:33 +0200

    redirect to cpn index when logging out

--- app/epa/settings.py
+++ app/epa/settings.py
@@ -158,7 +158,7 @@ AUTH_USER_MODEL = "users.CustomUser"
 
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "project_search"
-LOGOUT_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home_cpn"
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
