4ada0131f40202b1bead9811ecef89377c96fd61
Alexis Michaltsis <a.michaltsis@gmail.com>
2022-11-08 16:17:23 +0100

    Add self-service user account deletion

--- app/users/urls.py
+++ app/users/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 
-from .views import signup, change_password, user_info, activate, password_reset_request
+from .views import (
+    signup,
+    change_password,
+    user_info,
+    activate,
+    password_reset_request,
+    user_deletion_request,
+)
 
 urlpatterns = [
     path("signup/", signup, name="signup"),
@@ -8,4 +15,5 @@ urlpatterns = [
     path("user_info/", user_info, name="user_info"),
     path("activate/<uidb64>/<token>/", activate, name="activate"),
     path("password_reset", password_reset_request, name="password_reset"),
+    path("user_deletion", user_deletion_request, name="user_deletion"),
 ]
