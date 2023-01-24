5c8457d0c3558e87a52ae5d3f12cd7795affda83
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2023-01-24 12:08:57 +0100

    Renewable Ninjas API WIP

--- app/cp_nigeria/forms.py
+++ app/cp_nigeria/forms.py
@@ -1,32 +1,5 @@
-import pickle
-import os
-import json
-import io
-import csv
-from openpyxl import load_workbook
-
-from crispy_forms.bootstrap import AppendedText, PrependedText, FormActions
-from crispy_forms.helper import FormHelper
-from crispy_forms.layout import (
-    Submit,
-    Layout,
-    Row,
-    Column,
-    Field,
-    Fieldset,
-    ButtonHolder,
-)
-from django import forms
-from django.forms import ModelForm
-from django.core.validators import MaxValueValidator, MinValueValidator
 from django.utils.translation import ugettext_lazy as _
-from django.conf import settings as django_settings
-from projects.models import *
-from projects.constants import MAP_EPA_MVS, RENEWABLE_ASSETS
 from projects.forms import *
-
-from dashboard.helpers import KPI_PARAMETERS_ASSETS, KPIFinder
-from projects.helpers import parameters_helper, PARAMETERS
 
 CURVES = (("Evening Peak", "Evening Peak"),
           ("Midday Peak", "Midday Peak"))
@@ -34,6 +7,7 @@ CURVES = (("Evening Peak", "Evening Peak"),
 TIERS = (("Tier 1", "Tier 1"),
          ("Tier 2", "Tier 2"),
          ("Tier 3", "Tier 3"))
+
 
 class CPNLocationForm(ProjectCreateForm):
     weather = forms.FileField(
