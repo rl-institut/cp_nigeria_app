1a3b188bbd82159244250138a7b4b4da9ab3f6ae
paulapreuss <p.preuss@tu-berlin.de>
2023-02-05 13:43:04 +0100

    populate database with initial facility data

--- app/initial_setup.sh
+++ app/initial_setup.sh
@@ -4,5 +4,6 @@ python manage.py makemigrations users projects dashboard && \
 python manage.py migrate && \
 python manage.py update_assettype && \
 python manage.py loaddata 'fixtures/multivector_fixture.json' && \
+python manage.py loaddata 'fixtures/cp_initial_data.json' && \
 python manage.py collectstatic --no-input && \
 echo 'Completed initial setup of open-plan GUI app successfully!!'
