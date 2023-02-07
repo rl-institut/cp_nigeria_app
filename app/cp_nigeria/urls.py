bee365aa6e65f65e1f98f77d1560f3e5ad14a4e1
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2023-02-07 13:48:27 +0100

    Add a user-group div with unique id
    And link its content to an ajax post view

--- app/cp_nigeria/urls.py
+++ app/cp_nigeria/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     ),
     path("<int:proj_id>/review/<int:scen_id>", cpn_review, name="cpn_review"),
     # path("<int:proj_id>/update/energy/system/<int:scen_id>", update_energy_system, name="update_energy_system"),
+    path("ajax/usergroup/form", ajax_usergroup_form, name="ajax_usergroup_form"),
 ]
