76df66004438ad145c97213ba4daaa3cf39d9b5d
paulapreuss <paula.preuss@rl-institut.de>
2023-08-14 14:31:11 +0200

    fix dependent dropdown upon base consumer selection

--- app/static/js/dependent_dropdown.js
+++ app/static/js/dependent_dropdown.js
@@ -4,6 +4,10 @@ source: https://simpleisbetterthancomplex.com/tutorial/2018/01/29/how-to-impleme
 
     $(document).on('change', '#id_user_type', function () {
       var user_typeId = $(this).val();  // get the selected user_type ID from the HTML input
+      var parentTr = $(this).closest('tr'); // Get the parent table row of the changed input
+      // Find the corresponding facility_type input within the same div
+      var facilityTypeInput = parentTr.find('#id_facility_type');
+
 
       $.ajax({                       // initialize an AJAX request
         url: dropdownUrl,                    // set the url of the request (= localhost:8000/hr/ajax/load-facilities/)
@@ -11,7 +15,7 @@ source: https://simpleisbetterthancomplex.com/tutorial/2018/01/29/how-to-impleme
           'user_type': user_typeId       // add the user_type id to the GET parameters
         },
         success: function (data) {   // `data` is the return of the `load_facilities` view function
-          $("#id_facility_type").html(data);  // replace the contents of the facility_type input with the data that came from the server
+          facilityTypeInput.html(data);  // replace the contents of the facility_type input with the data that came from the server
         }
       });
 
