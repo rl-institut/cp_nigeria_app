586173dfda5a5918979272aa10319fca0bc67984
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2023-09-11 15:48:39 +0200

    Update forms.py to Django >= 4

--- app/cp_nigeria/forms.py
+++ app/cp_nigeria/forms.py
@@ -1,6 +1,6 @@
 from django import forms
-from django.utils.translation import ugettext_lazy as _
 from projects.forms import OpenPlanModelForm, ProjectCreateForm
+from django.utils.translation import gettext_lazy as _
 
 from projects.forms import StorageForm, AssetCreateForm, UploadTimeseriesForm
 
