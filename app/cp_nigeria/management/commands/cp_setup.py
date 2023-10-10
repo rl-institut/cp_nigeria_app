c30101662123b5890355254866b67136eed48356
paulapreuss <paula.preuss@rl-institut.de>
2023-10-10 16:56:08 +0200

    base command to set up cp models (deletes old db entries and re-loads them)

---
+++ app/cp_nigeria/management/commands/cp_setup.py
@@ -0,0 +1,29 @@
+from django.core.management.base import BaseCommand
+from django.core.management import call_command
+from cp_nigeria.models import DemandTimeseries, Community, ConsumerType, ConsumerGroup
+from projects.models import Timeseries
+
+
+class Command(BaseCommand):
+    help = "Create base cases for communities of practice"
+
+    def handle(self, *args, **options):
+        # delete existing entries
+        communities = ["Ebute-Ipare", "Ezere","Usungwe","Unguwar Kure", "Egbuniwa (Okpanam)"]
+        for model in [DemandTimeseries, Community, ConsumerType]:
+            qs = model.objects.all()
+            if qs.exists():
+                qs.delete()
+
+        pv_ts = [f"{community} PV Output" for community in communities]
+        qs = Timeseries.objects.filter(name__in=pv_ts)
+        if qs.exists():
+            qs.delete()
+
+        qs = ConsumerGroup.objects.filter(community__isnull=False)
+        if qs.exists():
+            qs.delete()
+
+        call_command("loaddata", "fixtures/peoplesun_demand_profiles.json")
+        call_command("loaddata", "fixtures/cp_setup.json")
+
