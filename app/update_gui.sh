b190327158b90441470172514fd51e158faa11b9
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2023-10-14 23:00:52 +0200

    Change python to python3

--- app/update_gui.sh
+++ app/update_gui.sh
@@ -1,6 +1,6 @@
-#!/usr/local/bin/python
-python manage.py compilemessages
-python manage.py makemigrations users projects dashboard && \
-python manage.py migrate && \
-python manage.py collectstatic && \
+#!/usr/local/bin/python3
+python3 manage.py compilemessages
+python3 manage.py makemigrations users projects dashboard && \
+python3 manage.py migrate && \
+python3 manage.py collectstatic && \
 echo 'Updated the open-plan GUI app successfully!!'
