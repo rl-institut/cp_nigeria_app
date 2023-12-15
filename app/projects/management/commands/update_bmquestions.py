87bfaca042f1810b7300fc40ad9f50fabbe937a1
pierre-francois.duc <pierre-francois.duc@rl-institut.de>
2023-12-15 16:41:19 +0100

    Add field sub_question to business model selection questions

--- app/projects/management/commands/update_bmquestions.py
+++ app/projects/management/commands/update_bmquestions.py
@@ -1,10 +1,11 @@
 from django.core.management.base import BaseCommand
 import pandas as pd
+import numpy as np
 from business_model.models import *
 
 
 class Command(BaseCommand):
-    help = "Update the assettype objects from /static/capacities_list.csv"
+    help = "Update the business question objects from /static/business_model_questions.csv"
 
     def add_arguments(self, parser):
         parser.add_argument("--update", action="store_true", help="Update existing capacities")
@@ -18,6 +19,8 @@ class Command(BaseCommand):
             question_id = asset_params.pop("question_index")
             qs = BMQuestion.objects.filter(id=question_id)
             asset_params["score_allowed_values"] = asset_params["score_allowed_values"].replace("'", '"')
+            if pd.isna(asset_params["sub_question_to"]):
+                asset_params.pop("sub_question_to")
             if qs.exists() is False:
                 new_asset = BMQuestion(**asset_params)
                 new_asset.save()
