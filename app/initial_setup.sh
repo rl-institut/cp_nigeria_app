acddd2570c26f281d1db030bcb6639b9993daee6
stefansc1 <stefan.schirmeister@localiser.de>
2025-10-15 10:41:49 +0200

    remove migrations from gitignore and setup scripts

--- app/initial_setup.sh
+++ app/initial_setup.sh
@@ -1,6 +1,5 @@
 #!/usr/local/bin/python
 python manage.py compilemessages
-python manage.py makemigrations users projects dashboard && \
 python manage.py migrate && \
 python manage.py loaddata 'fixtures/fixture.json' && \
 python manage.py collectstatic --no-input && \
